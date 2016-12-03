4ef606fc540a22c265f069e07de69ee3ba040b30
Joe Izenman <jizenman@sitecrafting.com>
2016-12-02 17:37:16 -0800

    updating views

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from flask import render_template
+from flask import request
 from app import app
 from .forms import PitcherForm, BatterForm, InjuryForm, WindowForm
+from stats import batter
 
 @app.route('/')
 @app.route('/index')
@@ -18,3 +20,15 @@ def index():
 			   batterform = batterform,
 			   injuryform = injuryform,
 			   windowform = windowform)
+
+
+@app.route('/injury/batter')
+def batter():
+    inj_id = request.args.get("inj_id")
+    window = request.args.get("window")
+
+    s = batter.prepost_aggregate_stats(int(inj_id), int(window))
+
+    #pre_slash = stats["pre"]
+    #post_slash = stats["post"]
+    return render_template('batter.html', title='Batter', pre_slash=inj_id, post_slash=window)
--- site.wsgi
+++ site.wsgi
@@ -1,4 +1,6 @@
-import sys
+import sys, logging
 sys.path.insert(0, '/var/www/injuryfx')
 
 from app import app as application
+
+logging.basicConfig(filename='/var/log/flask/error.log',level=logging.DEBUG)
