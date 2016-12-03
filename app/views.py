7e29d34068fb53eb38d854753686dcdeba87898e
Joe Izenman <jizenman@sitecrafting.com>
2016-12-03 10:22:00 -0800

    add pitcher view and stats functions

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from flask import request
 from app import app
 from .forms import PitcherForm, BatterForm, InjuryForm, WindowForm
 from stats import batter as b
+from stats import pitcher as p
 from injury import injury
 
 @app.route('/')
@@ -31,13 +32,14 @@ def batter():
     inj = injury.get_injury(inj_id)
     s = b.prepost_aggregate_stats(int(inj_id), int(window))
 
-    pre = {}
-    post = {}
-
-    pre["stats"] = b.slash_line(s["pre"])
-    post["stats"] = b.slash_line(s["post"])
-    pre["image_path"] = "/static/images/figure_1.png"
-    post["image_path"] = "/static/images/figure_1.png"
+    pre = {
+        "stats": "Slash Line: "+b.slash_line(s["pre"]),
+        "image_path": "/static/images/figure_1.png"
+    }
+    post = {
+        "stats": "Slash Line: "+b.slash_line(s["post"]),
+        "image_path": "/static/images/figure_1.png"
+    }
 
     # inj = {
     #     "player_name": "Bryce Harper",
@@ -54,3 +56,23 @@ def batter():
     # post["stats"] = "Stuff"
 
     return render_template('prepost.html', title='Batter', pre=pre, post=post, inj=inj)
+
+
+@app.route('/injury/pitcher')
+def pitcher():
+    inj_id = request.args.get("inj_id")
+    window = request.args.get("window")
+
+    inj = injury.get_injury(inj_id)
+    s = p.prepost_aggregate_stats(int(inj_id), int(window))
+
+    pre = {
+        "stats": b.slash_line("Opposing Slash Line: "+s["pre"]),
+        "image_path": "/static/images/figure_1.png"
+    }
+    post = {
+        "stats": b.slash_line("Opposing Slash Line: "+s["post"]),
+        "image_path": "/static/images/figure_1.png"
+    }
+
+    return render_template('prepost.html', title='Pitcher', pre=pre, post=post, inj=inj)
