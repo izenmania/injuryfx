9516f4b65e8f7ba8296606d5e7f2d3c6c976fa8d
cheapcontact <skessler@gmail.com>
2016-12-04 06:31:26 +0000

    create graph based on injury_id and window

--- stats/graphics.py
+++ stats/graphics.py
@@ -1,6 +1,10 @@
 import numpy as np
 from matplotlib.figure import Figure
 from matplotlib import cm as cm
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 
 def generate_heatmap(coords):
@@ -43,3 +47,42 @@ def generate_heatmap(coords):
     ax.plot([12,12],[18,42],'k')
 
     return fig
+
+def get_pitch_count_list(pitch_types, pitches_df):
+    'get a list of pinch counts that include 0 for pitches not in this dataframe'
+    counts = []
+    for pitch_type in pitch_types:
+       for row in pitches_df.iterrows():
+          if row[1]["pitch_type"] == pitch_type:
+             counts.append(row[1]["pitch_count"])
+             break
+       else:
+          counts.append(0)
+
+    return counts
+
+def create_bar_chart(pre_pitches_df, post_pitches_df, injury_id, window_size):
+    'Create graph comparing pre injury pitch and post injury pitch selection'
+    fig, ax = plt.subplots()
+    x = list(set(pre_pitches_df["pitch_type"]) | set(post_pitches_df["pitch_type"]))
+    n_groups = len(x)
+    pre_counts = get_pitch_count_list(x, pre_pitches_df)
+    post_counts = get_pitch_count_list(x, post_pitches_df)
+
+    bar_width = 0.35
+    opacity = .4
+    index = np.arange(n_groups)
+
+    rects1 = plt.bar(index, pre_counts, bar_width, alpha=opacity, color='r', label="Pre Injury")
+    rects2 = plt.bar(index + bar_width, post_counts, bar_width, alpha=opacity, color='b', label="Post Injury")
+
+    plt.xlabel('Pitch Type')
+    plt.ylabel('Count')
+    plt.xticks(index + bar_width, x)
+    plt.legend()
+    plt.title('Pitch Types. injury_id = ' + str(injury_id) + "; window size = " + str(window_size))
+
+
+    fig.savefig("/home/ubuntu/injuryfx/app/static/graphs/" + str(injury_id) + "-" +
+                str(window_size) + ".png") 
+
