92b6c746be67403f83dd6f08de3a78f642decfd0
Joe Izenman <jizenman@sitecrafting.com>
2016-12-10 15:50:39 -0800

    deleting extraneous comments

--- scripts/transaction-daily-load.py
+++ scripts/transaction-daily-load.py
@@ -5,14 +5,6 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from injury import raw, injury, parse
 from db import query
 
-# This process is intended to run every morning
-# yesterday = datetime.datetime.now().date() - timedelta(days=1)
-
-# filename = yesterday.strftime("%Y%m.json")
-# raw.load_raw(filename)
-# raw.append_raw(yesterday, yesterday)
-# raw.save_raw(filename)
-
 # Retrieve the last transaction saved, and the target conclusion date (typically last night)
 final_date = datetime.now().date() - timedelta(days=1)
 
