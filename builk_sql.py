64bddc3543943733df86c27e26b09acaa191ef34
cheapcontact <skessler@gmail.com>
2016-12-12 22:10:17 +0000

    renamed file

--- builk_sql.py
+++ builk_sql.py
@@ -8,7 +8,7 @@ def run_sql(sql):
     cur = conn.cursor(MySQLdb.cursors.DictCursor)
     cur.execute(sql, params)
 
-file = open('/tmp/flask.log', 'w+')
+file = open('/tmp/mysql-cache.log', 'w+')
 file.write("Starting flask. The system is safe to use now but slow. Starting caching.\n")
 file.flush()
 for y in [2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008]:
